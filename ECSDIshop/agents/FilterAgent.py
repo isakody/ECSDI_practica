14e603a9bfec2f4c3ddff3ef64515e0b3a216ad3
Isabel Codina <isakody@gmail.com>
2018-05-07 20:45:17 +0200

    Posibilidad de filtrar

--- ECSDIshop/agents/FilterAgent.py
+++ ECSDIshop/agents/FilterAgent.py
@@ -107,15 +107,14 @@ def communication():
 
     if messageProperties is None:
         # Respondemos que no hemos entendido el mensaje
-        resultadoComunicacion = build_message(Graph(), ACL['not-understood'], sender=FilterAgent.uri, msgcnt=getMessageCount())
+        resultadoComunicacion = build_message(Graph(), ACL['not-understood'],
+                                              sender=FilterAgent.uri, msgcnt=getMessageCount())
     else:
         # Obtenemos la performativa
         if messageProperties['performative'] != ACL.request:
             # Si no es un request, respondemos que no hemos entendido el mensaje
-            resultadoComunicacion = build_message(Graph(),
-                               ACL['not-understood'],
-                               sender=DirectoryAgent.uri,
-                               msgcnt=getMessageCount())
+            resultadoComunicacion = build_message(Graph(), ACL['not-understood'],
+                                                  sender=DirectoryAgent.uri, msgcnt=getMessageCount())
         else:
             # Extraemos el contenido que ha de ser una accion de la ontologia definida en Protege
             content = messageProperties['content']
@@ -123,21 +122,21 @@ def communication():
 
             # Si la acción es de tipo busqueda emprendemos las acciones consequentes
             if accion == ECSDI.BuscarProducto:
-                #Extraemos las restricciones de busqueda que se nos pasan y creamos un contenedor de las restriciones
+                # Extraemos las restricciones de busqueda que se nos pasan y creamos un contenedor de las restriciones
                 # para su posterior procesamiento
                 restricciones = grafoEntrada.objects(content, ECSDI.RestringidaPor)
                 directivasRestrictivas = {}
                 for restriccion in restricciones:
                     if grafoEntrada.value(subject=restriccion, predicate=RDF.type) == ECSDI.RestriccionDeNombre:
                         nombre = grafoEntrada.value(subject=restriccion, predicate=ECSDI.Nombre)
-                        directivasRestrictivas['Nombre'] = nombre;
+                        directivasRestrictivas['Nombre'] = nombre
                     elif grafoEntrada.value(subject=restriccion, predicate=RDF.type) == ECSDI.RestriccionDePrecio:
                         precioMax = grafoEntrada.value(subject=restriccion, predicate=ECSDI.PrecioMaximo)
                         precioMin = grafoEntrada.value(subject=restriccion, predicate=ECSDI.PrecioMinimo)
-                        directivasRestrictivas['PrecioMax'] = precioMax;
-                        directivasRestrictivas['PrecioMin'] = precioMin;
+                        directivasRestrictivas['PrecioMax'] = precioMax
+                        directivasRestrictivas['PrecioMin'] = precioMin
 
-                #llamamos a una funcion que nos retorna un grafo con la información acorde al filtro establecido por el usuario
+                # Llamamos a una funcion que nos retorna un grafo con la información acorde al filtro establecido por el usuario
                 resultadoComunicacion = findProductsByFilter(**directivasRestrictivas)
 
             logger.info('Respondemos a la petición de busqueda')
@@ -183,25 +182,82 @@ def tidyUp():
     pass
 
 #funcion llamada al principio de un agente
-def sellerBehavior(queue):
+def filterBehavior(queue):
 
     """
     Agent Behaviour in a concurrent thread.
     :param queue: the queue
     :return: something
     """
-    gr = register_message();
+    gr = register_message()
 
 # TODO implementar este metodo y base de datos tipo RDF acorde a la ontologia de Protege
 # Función que busca productos en la base de datos acorde a los filtros establecidos con anterioriad
-def findProductsByFilter(Nombre=None,PrecioMin=None,PrecioMax=sys.float_info.max):
-    emptyGrapth = Graph()
-    return emptyGrapth
+def findProductsByFilter(Nombre=None,PrecioMin=0.0,PrecioMax=sys.float_info.max):
+    graph = Graph()
+    ontologyFile = open('../data/ProductsDB.owl')
+    graph.parse(ontologyFile, format='turtle')
+
+    addAnd = False;
+
+    query = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX default: <http://www.owl-ontologies.com/ECSDIstore#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?Producto ?Nombre ?Precio ?Descripcion ?Id
+    where {
+        ?Producto rdf:type default:Producto .
+        ?Producto default:Nombre ?Nombre .
+        ?Producto default:Precio ?Precio .
+        ?Producto default:Descripcion ?Descripcion .
+        ?Producto default:Id ?Id .
+        FILTER("""
+
+    if Nombre is not None:
+        query += """?Nombre = '""" + Nombre + """'"""
+        addAnd = True
+
+
+
+    if PrecioMin is not None:
+        if addAnd:
+            query += """ && """
+            addAnd = False
+        query += """?Precio >= """ + str(PrecioMin)
+        addAnd = True
+
+
+    if PrecioMax is not None:
+        if addAnd:
+            query += """ && """
+            addAnd = False
+        query += """?Precio <= """ + str(PrecioMax)
+
+    query += """)}"""
+
+    graph_query = graph.query(query)
+    products_graph = Graph();
+    products_graph.bind('ECSDI', ECSDI)
+    for product in graph_query:
+        product_nombre = product.Nombre
+        product_precio = product.Precio
+        product_descripcion = product.Descripcion
+        print(product_nombre)
+        print(product_precio)
+        print(product_descripcion)
+        sujeto = product.Producto
+        products_graph.add((sujeto, RDF.type, ECSDI.Producto))
+        products_graph.add((sujeto, ECSDI.Nombre, Literal(product_nombre, datatype=XSD.string)))
+        products_graph.add((sujeto, ECSDI.Precio, Literal(product_precio, datatype=XSD.float)))
+        products_graph.add((sujeto, ECSDI.Descripcion, Literal(product_descripcion, datatype=XSD.string)))
+
+    return products_graph
 
 if __name__ == '__main__':
     # ------------------------------------------------------------------------------------------------------
     # Run behaviors
-    ab1 = Process(target=sellerBehavior, args=(queue,))
+    ab1 = Process(target=filterBehavior, args=(queue,))
     ab1.start()
 
     # Run server
