6e4bd5d33afd5f412eb31ef8015c53fa260ac881
Isabel Codina <isakody@gmail.com>
2018-05-09 19:46:38 +0200

    Tesorero Agent

---
+++ ECSDIshop/agents/TesoreroAgent.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+
+"""
+Agente usando los servicios web de Flask
+/comm es la entrada para la recepcion de mensajes del agente
+/Stop es la entrada que para el agente
+Tiene una funcion AgentBehavior1 que se lanza como un thread concurrente
+Asume que el agente de registro esta en el puerto 9000
+"""
+import argparse
+import socket
+import sys
+import threading
+from multiprocessing import Queue, Process
+from time import sleep
+
+from flask import Flask, request
+from rdflib import URIRef, XSD
+
+from utils.ACLMessages import *
+from utils.Agent import Agent
+from utils.FlaskServer import shutdown_server
+from utils.Logging import config_logger
+from utils.OntologyNamespaces import ECSDI
+__author__ = 'ECSDIstore'
+
+# Definimos los parametros de la linea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--open', help="Define si el servidor est abierto al exterior o no", action='store_true',
+                    default=False)
+parser.add_argument('--port', type=int, help="Puerto de comunicacion del agente")
+parser.add_argument('--dhost', default=socket.gethostname(), help="Host del agente de directorio")
+parser.add_argument('--dport', type=int, help="Puerto de comunicacion del agente de directorio")
+
+# Logging
+logger = config_logger(level=1)
+
+# parsing de los parametros de la linea de comandos
+args = parser.parse_args()
+
+# Configuration stuff
+if args.port is None:
+    port = 9009
+else:
+    port = args.port
+
+if args.open is None:
+    hostname = '0.0.0.0'
+else:
+    hostname = socket.gethostname()
+
+if args.dport is None:
+    dport = 9000
+else:
+    dport = args.dport
+
+if args.dhost is None:
+    dhostname = socket.gethostname()
+else:
+    dhostname = args.dhost
+
+# AGENT ATTRIBUTES ----------------------------------------------------------------------------------------
+
+# Agent Namespace
+agn = Namespace("http://www.agentes.org#")
+
+# Message Count
+mss_cnt = 0
+
+# Data Agent
+# Datos del Agente
+TesoreroAgent = Agent('TesoreroAgent',
+                    agn.TesoreroAgent,
+                    'http://%s:%d/comm' % (hostname, port),
+                    'http://%s:%d/Stop' % (hostname, port))
+
+# Directory agent address
+DirectoryAgent = Agent('DirectoryAgent',
+                       agn.Directory,
+                       'http://%s:%d/Register' % (dhostname, dport),
+                       'http://%s:%d/Stop' % (dhostname, dport))
+
+# Global triplestore graph
+dsGraph = Graph()
+
+# Queue
+queue = Queue()
+
+# Flask app
+app = Flask(__name__)
+
+#función inclremental de numero de mensajes
+def getMessageCount():
+    global mss_cnt
+    mss_cnt += 1
+    return mss_cnt
+
+#funcion llamada en /comm
+@app.route("/comm")
+def communication():
+    message = request.args['content']
+    grafoEntrada = Graph()
+    grafoEntrada.parse(data=message)
+
+    messageProperties = get_message_properties(grafoEntrada)
+
+    resultadoComunicacion = Graph()
+
+    if messageProperties is None:
+        # Respondemos que no hemos entendido el mensaje
+        resultadoComunicacion = build_message(Graph(), ACL['not-understood'],
+                                              sender=TesoreroAgent.uri, msgcnt=getMessageCount())
+    else:
+        # Obtenemos la performativa
+        if messageProperties['performative'] != ACL.request:
+            # Si no es un request, respondemos que no hemos entendido el mensaje
+            resultadoComunicacion = build_message(Graph(), ACL['not-understood'],
+                                                  sender=DirectoryAgent.uri, msgcnt=getMessageCount())
+        else:
+            # Extraemos el contenido que ha de ser una accion de la ontologia definida en Protege
+            content = messageProperties['content']
+            accion = grafoEntrada.value(subject=content, predicate=RDF.type)
+            # Si la acción es de tipo peticiónCompra emprendemos las acciones consequentes
+            if accion == ECSDI.PeticionTransferencia:
+                logger.info("Procesando peticion de transferencia")
+                # Eliminar los ACLMessage
+                for item in grafoEntrada.subjects(RDF.type, ACL.FipaAclMessage):
+                    grafoEntrada.remove((item, None, None))
+
+                procesarTransferencia(grafoEntrada)
+
+    #no retronamso nada
+    logger.info('Respondemos a la petición de venta')
+    serialize = resultadoComunicacion.serialize(format='xml')
+    return serialize, 200
+
+
+
+def procesarTransferencia(grafo):
+    transferencia = grafo.value(predicate=RDF.type,object=ECSDI.PeticionTransferencia)
+    tarjeta = grafo.value(subject=transferencia, predicate=ECSDI.Tarjeta)
+    precio = grafo.value(subject=transferencia, predicate=ECSDI.PrecioTotal)
+    logger.info("Cobrando a " + tarjeta + " " + precio + " euros.")
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint to the agent
+    :return: string
+    """
+
+    tidyUp()
+    shutdown_server()
+    return "Stopping server"
+
+#funcion llamada al principio de un agente
+def enviadorBehavior(queue):
+
+    """
+    Agent Behaviour in a concurrent thread.
+    :param queue: the queue
+    :return: something
+    """
+    gr = register_message()
+
+
+#función llamada antes de cerrar el servidor
+def tidyUp():
+    """
+    Previous actions for the agent.
+    """
+
+    global queue
+    queue.put(0)
+
+    pass
+
+#función para registro de agente en el servicio de directorios
+def register_message():
+    """
+    Envia un mensaje de registro al servicio de registro
+    usando una performativa Request y una accion Register del
+    servicio de directorio
+
+    :param gmess:
+    :return:
+    """
+
+    logger.info('Nos registramos')
+
+    gr = registerAgent(TesoreroAgent, DirectoryAgent, TesoreroAgent.uri, getMessageCount())
+    return gr
+
+def comprobarYCobrar():
+    print("Te estoi cobrando guei")
+    return
+def cobrar():
+    comprobarYCobrar()
+    threading.Timer(10, cobrar).start()
+cobrar()
+
+
+
+if __name__ == '__main__':
+    # ------------------------------------------------------------------------------------------------------
+    # Run behaviors
+    ab1 = Process(target=enviadorBehavior, args=(queue,))
+    ab1.start()
+
+    # Run server
+    app.run(host=hostname, port=port, debug=True)
+
+    # Wait behaviors
+    ab1.join()
+    cobrar()
+    print('The End')
