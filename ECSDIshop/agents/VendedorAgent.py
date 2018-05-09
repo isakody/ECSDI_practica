64a09ab879efaf5c77227e17dd7864d20ec6f6f9
Isabel Codina <isakody@gmail.com>
2018-05-09 20:24:58 +0200

    PrecioTotal en petición de compra

--- ECSDIshop/agents/VendedorAgent.py
+++ ECSDIshop/agents/VendedorAgent.py
@@ -169,7 +169,7 @@ def communication():
 
                 factura += """TOTAL:  """ + str(precioTotal)
                 grafoFactura.add((sujeto, ECSDI.PrecioTotal, Literal(precioTotal, datatype=XSD.float)))
-                grafoEntrada.add((relacion, ECSDI.PrecioTotal, Literal(precioTotal, datatype=XSD.float)))
+                grafoEntrada.add((accion, ECSDI.PrecioTotal, Literal(precioTotal, datatype=XSD.float)))
                 thread = Process(target=enviarCompra, args=(grafoEntrada, content))
                 thread.start()
 
